cc123ff6ccee25c05cd36516ff6cda197b5924be
tatsuya98 <nathanbailey_62@hotmail.com>
2024-07-16 14:44:27 +0100

    POST /api/articles/:article_id/comments added tests for post request added logic to errorHandling return everything after updating instead of just the body

--- errorHandling/errorHandling.js
+++ errorHandling/errorHandling.js
@@ -9,12 +9,21 @@ exports.SqlErrorHandling = (error, request, response, next) => {
   if (error.code === "22P02") {
     response.status(400).send({ message: "Bad request" });
   }
-  if (error.code === "23503") {
+  if (error.code === "23503" && error.detail.includes("articles")) {
+    response.status(422).send({
+      message: "unable to post comment to an article that does not exist",
+    });
+  }
+  if (error.code === "23503" && error.detail.includes("users")) {
+    response.status(404).send({
+      message: "username does not exist",
+    });
+  }
+
+  if (error.code === "23502") {
     response
-      .status(422)
-      .send({
-        message: "unable to post comment to an article that does not exist",
-      });
+      .status(400)
+      .send({ message: "request is missing 1 or more fields" });
   } else {
     next(error);
   }
