b8edc1dfa8cc98aa686336ae9bf8ff1538e0b427
tatsuya98 <nathanbailey_62@hotmail.com>
2024-07-18 13:42:11 +0100

    /api/users/:username completed endpoint

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,7 +1,15 @@
-const { fetchUsers } = require("../models/users.models");
+const { fetchUsers, fetchUserByUsername } = require("../models/users.models");
 
 exports.getUsers = (request, response) => {
   return fetchUsers().then((users) => {
     response.status(200).send({ users });
   });
+};
+exports.getUserByUsername = (request, response, next) => {
+  const { username } = request.params;
+  return fetchUserByUsername(username)
+    .then((user) => {
+      response.status(200).send({ user });
+    })
+    .catch((error) => next(error));
 };
