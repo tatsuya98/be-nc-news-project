22af3f03982e82cfa0e49fc2113ff19c95d8afe7
tatsuya98 <nathanbailey_62@hotmail.com>
2024-08-05 09:57:01 +0100

    added cors

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const {
   customErrorHandling,
   serverErrorHandling,
 } = require("./errorHandling/errorHandling");
+const cors = require("cors");
 const apiRouter = require("./routes/api-router");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", apiRouter);
 app.use(customErrorHandling);
